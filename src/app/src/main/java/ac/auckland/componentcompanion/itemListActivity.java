39b0c53f8d3f4386d93dc85158424d3900cc3011
klek990 <68366597+klek990@users.noreply.github.com>
2021-05-21 23:51:43 +1200

    Created skeleton files for each activity

---
+++ src/app/src/main/java/ac/auckland/componentcompanion/itemListActivity.java
@@ -0,0 +1,13 @@
+package ac.auckland.componentcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+    }
+}
