581f07de3b479645840060b8b414e8e007f3fa6c
klek990 <68366597+klek990@users.noreply.github.com>
2021-05-31 13:00:49 +1200

    Added skeleton code for search queries and also added back button remap

--- src/app/src/main/java/ac/auckland/componentcompanion/MainActivity.java
+++ src/app/src/main/java/ac/auckland/componentcompanion/MainActivity.java
@@ -1,5 +1,10 @@
 package ac.auckland.componentcompanion;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +13,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        Button cat0Btn = findViewById(R.id.cat0Btn);
+
+        cat0Btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent catActivity = new Intent(MainActivity.this, itemListActivity.class);
+                startActivity(catActivity);
+            }
+        });
+
+        SearchView searchBar = findViewById(R.id.searchBar);
+
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                // Filter results in here with adapter
+                return false;
+            }
+        });
+
+
+
+
     }
 }
--- src/app/src/main/java/ac/auckland/componentcompanion/itemListActivity.java
+++ src/app/src/main/java/ac/auckland/componentcompanion/itemListActivity.java
@@ -1,5 +1,10 @@
 package ac.auckland.componentcompanion;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,16 @@ public class itemListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        // Back button remap, goes to previous activity
+        OnBackPressedCallback backButtonCall = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+            }
+        };
+       getOnBackPressedDispatcher().addCallback(this, backButtonCall);
+
     }
+
 }
